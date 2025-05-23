b96defe9cc8f4fe2d5aee5ace3d5b98139a480d1
Renan Reis <rrgdosreis@gmail.com>
2025-05-23 11:43:45 -0300

    Alinhamento safado

--- src/pages/relatorios/index.jsx
+++ src/pages/relatorios/index.jsx
@@ -353,7 +353,7 @@ const Dashboard = () => {
           </Box>
 
 
-          <Box direction={'column'} height={'494px'} flex={'1'}>
+          <Box direction={'column'} height={'490px'} flex={'1'}>
             <ChartsWrapper flex={'1'} height={'100%'}>
               <TextDefault bottom={'15px'} weight={'bold'} color={colors.black}>
                 Previsão de Economia:
